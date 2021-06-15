43d95032bb652ffabfb1507c575dd0c97eb6de56
Alex <fox228.af@gmail.com>
2021-06-15 19:15:17 +0300

    Methods and props

--- Workweek.cs
+++ Workweek.cs
@@ -8,18 +8,33 @@ using System.Threading.Tasks;
 namespace gratch_core
 {
     /// <summary>
-    /// Класс для работы с рабочей неделей
+    /// Класс для работы с рабочей неделей.
+    /// Выходных должно быть не больше 6.
     /// </summary>
     internal class Workweek
     {
+        //Constants
+        private const int daysInWeek = 7;
         //Fields
         /// <summary>
         /// <see cref="local"/> - файл с локализацией. local.GetString(DayOfWeek.*);
         /// </summary>
-        private readonly ResourceManager local = new("dayofweek-ru",
+        private readonly ResourceManager local = new("dayofweek-ru", // TODO: полноценные переводы
             typeof(Group).Assembly);
         private Dictionary<DayOfWeek, bool> week;
         //Properties
+        public int CountHolidays
+        {
+            get {
+                int counter = 0;
+                foreach(DayOfWeek day in week.Keys)
+                {
+                    if (week[day] == true) counter++;
+                }
+                return counter;
+            }
+        }
+        public int CountWorkDays => daysInWeek - CountHolidays;
         public Dictionary<DayOfWeek, bool> Week => week;
         //Constructors
         /// <summary>
@@ -43,7 +58,24 @@ namespace gratch_core
             foreach (DayOfWeek day in weekend) week[day] = true;
         }
         //Methods
-        public bool IsWeekend(DayOfWeek day) => week[day];
+        /// <summary>
+        /// Проверяет выходной ли <paramref name="day"/>
+        /// </summary>
+        /// <param name="day">День недели</param>
+        /// <returns>
+        /// <c>true</c> если <paramref name="day"/> выходной, иначе - <c>false</c>
+        /// </returns>
+        public bool IsHoliday(DayOfWeek day) => week[day];
         public string DayToString(DayOfWeek day) => local.GetString(day.ToString());
+        public void AddHoliday(DayOfWeek day)
+        {
+            //Ошибка когда вся неделя выходная
+            if (CountHolidays >= 6) throw new ArgumentOutOfRangeException(nameof(day), "Слишком много выходных");
+            if (!IsHoliday(day)) week[day] = true;
+        }
+        public void AddWorkday(DayOfWeek day)
+        {
+            if(IsHoliday(day)) week[day] = false;
+        }
     }
 }
