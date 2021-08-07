7310a3ea1a8276f7d1a4476baf38b9680aed94e5
Alex <fox228.af@gmail.com>
2021-08-07 16:59:13 +0300

    Rewrite, tests

--- Group.cs
+++ Group.cs
@@ -1,49 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Resources;
-
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace gratch_core
 {
-    /// <summary>
-    /// Класс для работы с группами юнитов.
-    /// </summary>
     public class Group
     {
-        //Fields
-        private string name;
-        private Workweek workweek;
-        private List<FixedUnit> units;
-        //Properties
-        public string Name
+        public List<Person> People { get; set; } = new();
+        public List<DayOfWeek> weekend { get; set; } = new();
+        public Group()
         {
-            get => name;
-            set
-            {
-                if (value != null && value.Length > 2) name = value;
-            }
-        }
-        public Workweek Workweek { get => workweek; internal set => workweek = value; }
-        public List<FixedUnit> Units { get => units; internal set => units = value; }
-        //Constructors      
-        public Group(string groupName) : this(groupName,new Workweek())
-        {
-
-        }
-        public Group(string groupName, Workweek workweek) : this(groupName,workweek,null)
-        {
-        
-        }
-        public Group(string groupName, Workweek workweek, List<FixedUnit> units)
-        {
-            if (groupName != null && groupName.Length > 2) name = groupName;
-            this.workweek = workweek;
-            this.units = units;
-        }
-        //Methods
-        void AddUnit(FixedUnit unit)
-        {
-            unit.Group = this;
+            
         }
     }
 }
---
+++ Person.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace gratch_core
+{
+    public class Person
+    {
+        public string Name { get; set; }
+        public DateTime DutyDate { get; set; }
+
+        public bool IsOnWeekend(ref Group group)
+        {
+            return group.weekend.Contains(DutyDate.DayOfWeek);
+        }
+    }
+}
