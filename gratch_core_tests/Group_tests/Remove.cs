489b030e21538c5931f6d7bd7ca7e9f76ebba9ec
Alex <fox228.af@gmail.com>
2021-08-16 21:19:14 +0300

    Remove - Default

---
+++ gratch_core_tests/Group_tests/Remove.cs
@@ -0,0 +1,29 @@
+﻿using gratch_core;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace gratch_core_tests.Group_tests
+{
+    [TestClass]
+    public class Remove
+    {
+        [TestMethod]
+        public void Default()
+        {
+            const int index = 9;
+
+            var group = DataFiller.GetGroup(20);
+            var deletedPerson = group.People[index];
+
+            group.Remove(index);
+
+            Assert.IsFalse(group.People.Contains(deletedPerson));
+        }
+    }
+}
