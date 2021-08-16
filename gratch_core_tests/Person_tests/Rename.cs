3de4a18c38412b878e392cb0266e22f948856442
Alex <fox228.af@gmail.com>
2021-08-16 20:01:38 +0300

    Succesful Rename Test

---
+++ gratch_core_tests/Person_tests/Rename.cs
@@ -0,0 +1,28 @@
+﻿using gratch_core;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace gratch_core_tests.Person_tests
+{
+    [TestClass]
+    public class Rename
+    {
+        
+        [TestMethod]
+        public void Succesful()
+        {
+            var group = DataFiller.GetGroup(20);
+            var name = "Gosha";
+
+            group.People[0].Rename(name);
+
+            Assert.AreEqual(name, group.People[0].Name);
+        }
+    }
+}
