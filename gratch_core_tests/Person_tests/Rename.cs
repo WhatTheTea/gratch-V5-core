4607ae4f84b1f99ba120184cc2a29c35f9c5495e
Alex <fox228.af@gmail.com>
2021-08-16 20:01:56 +0300

    Conflict Rename Test

--- gratch_core_tests/Person_tests/Rename.cs
+++ gratch_core_tests/Person_tests/Rename.cs
@@ -13,7 +13,16 @@ namespace gratch_core_tests.Person_tests
     [TestClass]
     public class Rename
     {
-        
+        [TestMethod]
+        public void Conflict()
+        {
+            var group = DataFiller.GetGroup(20);
+            var name = group.People[0].Name;
+
+            group.People[4].Rename(name);
+
+            Assert.AreNotEqual(name, group.People[4]);
+        }
         [TestMethod]
         public void Succesful()
         {
