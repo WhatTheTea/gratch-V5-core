aa5e6d95b118a55b5fa9b8f7e025904c57c8ca62
Alex <fox228.af@gmail.com>
2021-08-17 20:56:32 +0300

    Graph.DutyDates default test

---
+++ gratch_core_tests/Graph-tests/DutyDates.cs
@@ -0,0 +1,26 @@
+﻿using gratch_core;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using System;
+
+namespace gratch_core_tests {
+    [TestClass]
+    public class DutyDates
+    {
+        [TestMethod]
+        public void Default()
+        {
+            const int num1 = 1;
+            const int num2 = 40;
+
+            int dinm = DateTime.Now.DaysInMonth();
+
+            var group1 = DataFiller.GetGroup(num1);
+            var group2 = DataFiller.GetGroup(num2);
+
+            Assert.AreEqual(dinm, group1.Graph.DutyDates.Count);
+            Assert.AreEqual(dinm, group2.Graph.DutyDates.Count);
+        }
+    }
+}
