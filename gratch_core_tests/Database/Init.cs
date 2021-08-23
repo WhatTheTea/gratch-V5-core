3dcf793f2eee920d4f5f73d6a4006c2eff0cbbcb
Alex <fox228.af@gmail.com>
2021-08-23 17:08:14 +0300

    DB.ReadOperations test

--- gratch_core_tests/Database/Init.cs
+++ gratch_core_tests/Database/Init.cs
@@ -2,6 +2,9 @@
 
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
+using System.Collections.Generic;
+using System.Linq;
+
 namespace gratch_core_tests.Database
 {
     [Microsoft.VisualStudio.TestTools.UnitTesting.TestClass]
@@ -23,6 +26,26 @@ namespace gratch_core_tests.Database
             {
                 Assert.AreEqual(expected.DutyDates[i], actual.DutyDates[i]);
             }
+        }
+        [TestMethod] //!Должен выполнятся после предыдущего
+        public void ReadGroups()
+        {
+            BasicReadWrite();
+            List<Group> groups = new();
+
+            var models = new gratch_core.Models.GroupRepository().GetGroups();
+            foreach (var grp in Group.AllInstances) grp.Clear();
+            foreach (var mod in models) groups.Add(mod.ToGroup());
+
+            groups.ForEach(grp => Assert.IsTrue(grp.Any()));
+            groups.ForEach(grp =>
+            {
+                foreach (var p in grp)
+                { 
+                    Assert.IsTrue(p.Name != null);
+                    Assert.IsTrue(p.DutyDates.Any());
+                }
+            });
         }
     }
 }
